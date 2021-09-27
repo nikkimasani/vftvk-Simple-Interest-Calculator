c28e2f78478a0c1730364232c455e727c9e90efb
nikkimasani <88842480+nikkimasani@users.noreply.github.com>
2021-09-27 14:26:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-function compute() {
-  // variable called "principal" and assigning it the value of the input element having id of "principal"
-  let principal = document.getElementById("principal").value;
+function compute()
+{
+    // retrieve the value of the principal element and validate
+    // the value should be positive and not empty.
+    var principalObj = document.getElementById("principal");
+    var principal = principalObj.value;
+    if(principal <= 0)
+    {
+        alert("Enter a positive number.");
+        principalObj.focus();
+        return;
+    }
 
-  // variable called "rate" and assigning it the value of the input element having id of "rate"
-  let rate = document.getElementById("rate").value;
+    // retrieve the value of the rate range element. no validation
+    // is needed as it is marked required and the values are bounded
+    var rate = document.getElementById("rate").value;
 
-  // variable called "years" and assigning it the value of the input element having id of "years"
-  let years = document.getElementById("years").value;
+    // retrieve the value of years element. no validation is needed as
+    // it is bounded by the options given.
+    var years = document.getElementById("years").value;
 
-  // variable called "interest" and assigning it the value of principal * years * rate / 100
-  let interest = principal * years * rate / 100
+    // perform computations
+    var interest = principal * years * rate / 100.0;
+    var year = new Date().getFullYear() + parseInt(years);
 
-  // Convert number of year into actual year in future (ie, from now on) by using Date() constructor
-  let futureYearFromNow = new Date();
-  futureYearFromNow.setFullYear(futureYearFromNow.getFullYear() + Number(years));
-
-  // Get the reference to the element named "result" and show output of interest
-  document.getElementById("result").innerHTML = `<b>${interest}</b> to be paid by year ${futureYearFromNow.getFullYear()}`;
-
+    // generate the text presenting the results of the computation.
+    var out = 'If you deposit <span class="hitext">' + principal + '</span>,<br/>'
+        + 'at an interest rate of <span class="hitext">' + rate + '%</span>.<br/>'
+        + 'You will receive an amount of <span class="hitext">' + interest + '</span>,<br/>'
+        + 'in the year <span class="hitext">' + year + '</span><br/>';
+    document.getElementById("result").innerHTML = out;
+        
 }
 
-// Function that reads value of range slider and shows the value in <span id="rangeOutput"> 
-function rangeSliderVal(e) {
-  e.preventDefault();
-  // Get live (current) value of range slider 
-  let rangeSliderValue = e.target.value;
-
-  // Display the above read value into <span id="rangeOutput">  element
-  document.getElementById("rangeOutput").innerHTML = `${rangeSliderValue}`;
+function updateRate()
+{
+    // updates the display value of interest rate whenever the range onchanged
+    // event is triggered.
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
 }
-
-// linking the function rangeSliderVal() with an "onchange" event on range
-// So, function rangeSliderVal() will run every time the value of slider is changed (ie, onchange event happens on range element)
-document.getElementById("slider").onchange = rangeSliderVal;
